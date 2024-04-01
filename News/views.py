9704aa4ec46abf8e920465cee4ec9726af4d4598
Pouya <popibani1379@gmail.com>
2024-04-01 23:44:17 +0330

    News app completed successfully.

--- News/views.py
+++ News/views.py
@@ -3,18 +3,30 @@ from django.utils.encoding import uri_to_iri
 from django.views.generic import ListView, DetailView
 from hitcount.views import HitCountDetailView
 
-from News.models import News
+from News.models import News, Category
 
 
 class AllNews(ListView):
     model = News
     context_object_name = 'news'
     template_name = 'News/news_list.html'
+    paginate_by = 9
 
     def get_queryset(self):
         news = News.objects.select_related('category', 'author').order_by('-created_at')
 
         return news
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        categories = Category.objects.all()
+
+        context['categories'] = categories
+
+        print(categories)
+
+        return context
 
 
 class NewsDetail(HitCountDetailView, DetailView):
@@ -30,7 +42,7 @@ class NewsDetail(HitCountDetailView, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        related_news = self.object.get_related_news(max_results=5)
+        related_news = self.object.get_related_news(max_results=3)
         latest_news = self.object.get_latest_news()
 
         context['related_news'] = related_news
@@ -48,6 +60,7 @@ class NewsByCategory(ListView):
     model = News
     context_object_name = 'news'
     template_name = 'News/news_by_category.html'
+    paginate_by = 9
 
     def get_queryset(self):
         slug = uri_to_iri(self.kwargs.get('slug'))
@@ -55,3 +68,14 @@ class NewsByCategory(ListView):
         news = get_list_or_404(News, category__slug=slug)
 
         return news
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        slug = self.kwargs.get('slug')
+
+        category = Category.objects.get(slug=slug)
+
+        context['category'] = category
+
+        return context
