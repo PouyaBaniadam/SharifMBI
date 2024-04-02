f2950448929a4729ee06ca304bf8d1fa7dcc5aa9
Pouya <popibani1379@gmail.com>
2024-04-02 11:22:51 +0330

    us url bug fix

--- Us/urls.py
+++ Us/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('about', views.About.as_view(), name='about'),
     path('contact', views.Contact.as_view(), name='contact'),
     path('customers', views.CustomerList.as_view(), name='customers'),
-    path('customer/<slug:slug>', views.CustomerDetail.as_view(), name='customer'),
+    path('customer/<slug>', views.CustomerDetail.as_view(), name='customer'),
     path('faq/detail/<slug>', views.FaqDetail.as_view(), name='faq'),
     path('faqs', views.FaqList.as_view(), name='faqs'),
 ]
