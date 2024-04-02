e386eefbcfd1eaefeb0b7b541010c3f9e03b1920
Pouya <popibani1379@gmail.com>
2024-04-02 10:11:59 +0330

    weblog url bug fix

--- Weblog/urls.py
+++ Weblog/urls.py
@@ -6,6 +6,6 @@ app_name = 'weblog'
 
 urlpatterns = [
     path('list', views.AllWeblogs.as_view(), name='all'),
-    path('detail/<slug:slug>', views.WeblogDetail.as_view(), name='detail'),
-    path('category/<slug:slug>', views.WeblogsByCategory.as_view(), name='by_category'),
+    path('detail/<slug>', views.WeblogDetail.as_view(), name='detail'),
+    path('category/<slug>', views.WeblogsByCategory.as_view(), name='by_category'),
 ]
